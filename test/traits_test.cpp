ec71fc5423dfbb67d02c6bfc6a851f84f32267f3
Artem Koton <kotonartem@protonmail.com>
2023-04-15 23:16:04 +0300

    Check iterator_category

--- test/traits_test.cpp
+++ test/traits_test.cpp
@@ -36,13 +36,20 @@ TEST(traits_test, range_categories) {
 }
 
 TEST(traits_test, col_iterator_member_types) {
-  EXPECT_TRUE((std::is_same_v<element, matrix<element>::col_iterator::value_type>));
-  EXPECT_TRUE((std::is_same_v<element&, matrix<element>::col_iterator::reference>));
-  EXPECT_TRUE((std::is_same_v<element*, matrix<element>::col_iterator::pointer>));
+  using col_iterator = matrix<element>::col_iterator;
+  using const_col_iterator = matrix<element>::const_col_iterator;
 
-  EXPECT_TRUE((std::is_same_v<element, matrix<element>::const_col_iterator::value_type>));
-  EXPECT_TRUE((std::is_same_v<const element&, matrix<element>::const_col_iterator::reference>));
-  EXPECT_TRUE((std::is_same_v<const element*, matrix<element>::const_col_iterator::pointer>));
+  EXPECT_TRUE((std::is_same_v<element, col_iterator::value_type>));
+  EXPECT_TRUE((std::is_same_v<element&, col_iterator::reference>));
+  EXPECT_TRUE((std::is_same_v<element*, col_iterator::pointer>));
+  EXPECT_TRUE((std::is_same_v<std::ptrdiff_t, col_iterator::difference_type>));
+  EXPECT_TRUE((std::is_same_v<std::random_access_iterator_tag, col_iterator::iterator_category>));
+
+  EXPECT_TRUE((std::is_same_v<element, const_col_iterator::value_type>));
+  EXPECT_TRUE((std::is_same_v<const element&, const_col_iterator::reference>));
+  EXPECT_TRUE((std::is_same_v<const element*, const_col_iterator::pointer>));
+  EXPECT_TRUE((std::is_same_v<std::ptrdiff_t, const_col_iterator::difference_type>));
+  EXPECT_TRUE((std::is_same_v<std::random_access_iterator_tag, const_col_iterator::iterator_category>));
 }
 
 TEST(traits_test, iterator_triviality) {
