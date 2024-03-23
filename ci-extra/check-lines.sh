495199073a77483675e6f417f0d0010450ace900
Artem Koton <kotonartem@protonmail.com>
2024-03-23 22:13:18 +0300

    Bump template repository

--- ci-extra/check-lines.sh
+++ ci-extra/check-lines.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
+set -ou pipefail
 
 exit_code=0
 
-error()
-{
+error() {
   exit_code=1
   echo "Error in $1: ${*:2}" >&2
 }
 
 for file in $(git grep --cached -Il ''); do
-  if [[ -n "$(dos2unix < "$file" | cmp "$file")" ]]; then
+  if [[ -n "$(dos2unix <"$file" | cmp "$file")" ]]; then
     error "$file" "Found CRLF"
   fi
   if [[ -s "$file" && -n "$(tail -c 1 "$file")" ]]; then
